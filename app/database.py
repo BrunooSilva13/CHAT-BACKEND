1cf71bda644424d665ea173d669d08f510038133
Bruno <brunomendes.2259@gmail.com>
2025-05-17 14:48:26 -0300

    criando pasta app/

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, create_db
+from app.models import Base, create_db
 
 DATABASE_URL = "sqlite:///./chat.db"
 
